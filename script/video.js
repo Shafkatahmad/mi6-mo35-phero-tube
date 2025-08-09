9af113dc00197bbf551bcd19b00a1e9cdbeeeeb7
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-09 22:17:29 +0800

    fetch, load and display video grid from api

--- script/video.js
+++ script/video.js
@@ -14,6 +14,62 @@ const loadCategories = async() => {
   }
 }
 
+const loadVideos = async() => {
+  // fetch the data
+  try {
+    const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    const data = await res.json()
+    displayVideos(data.videos)
+  }
+  catch(error) {
+    console.error('Error fetching data;', error);
+  }
+}
+
+const cardDemo = {
+      "category_id": "1001",
+      "video_id": "aaaa",
+      "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+      "title": "Shape of You",
+      "authors": [
+        {
+          "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+          "profile_name": "Olivia Mitchell",
+          "verified": ""
+        }
+      ],
+      "others": {
+        "views": "100K",
+        "posted_date": "16278"
+      },
+      "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+    }
+
+const displayVideos = (videoss) => {
+  const videoContainer = document.getElementById('videos');
+  videoss.forEach( video => {
+    console.error(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = 
+    `
+    <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+    videoContainer.append(card);
+  })
+}
+
 // {
 //     "category_id": "1001",
 //     "category": "Music"
@@ -37,3 +93,4 @@ const displayCategories = (categories) => {
 }
 
 loadCategories();
+loadVideos();
